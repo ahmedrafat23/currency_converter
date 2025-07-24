e8ac6de1a2f1d5014a26b2029be2220e9a41d79a
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2025-07-24 11:35:52 +0300

    24/7/2025

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+API_KEY = '86d4ab9239a7e51aa9d81b64'
+BASE_URL = f'https://v6.exchangerate-api.com/v6/{API_KEY}/latest/'
+
+def get_rates(base):
+    url = BASE_URL + base
+    res = requests.get(url)
+    data = res.json()
+    if data['result'] == 'success':
+        return data['conversion_rates']
+    return None
+
+@app.route('/')
 def welcome():
-    return render_template("welcome.html")
+    return render_template('welcome.html')
 
-@app.route("/converter")
+@app.route('/converter', methods=['GET', 'POST'])
 def converter():
-    return render_template("converter.html")
+    result = None
+    currencies = []
+    amount = 0
+    from_currency = 'USD'
+    to_currency = 'EUR'
 
-if __name__ == "__main__":
+    if request.method == 'POST':
+        from_currency = request.form['from_currency']
+        to_currency = request.form['to_currency']
+        amount = float(request.form['amount'])
+
+        rates = get_rates(from_currency)
+        if rates and to_currency in rates:
+            result = round(amount * rates[to_currency], 2)
+        currencies = list(rates.keys()) if rates else []
+    else:
+        rates = get_rates('USD')
+        currencies = list(rates.keys()) if rates else []
+
+    return render_template(
+        'converter.html',
+        result=result,
+        currencies=currencies,
+        from_currency=from_currency,
+        to_currency=to_currency,
+        amount=amount
+    )
+
+if __name__ == '__main__':
     app.run(debug=True)
